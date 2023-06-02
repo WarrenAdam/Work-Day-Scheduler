ff050e3f7675e16c8b8e428155aabd47d0c86a99
WarrenAdam <130782045+WarrenAdam@users.noreply.github.com>
2023-06-02 13:15:13 -0400

    added function for refresh color change in past, present and future. As well as saving user inputs in text area to local storage.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,41 +2,61 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 const localeSettings = {};
-  dayjs.locale(localeSettings);
+dayjs.locale(localeSettings);
 
 $(function () {
- // Get the current hour of the day using the dayjs library.
- const currentHour = dayjs().format("H");
- // The function below changes the color of each time block based on whether it's in the "past, present, or future" relative to the current hour.
- function hourlyColor() {
-  $(".time-block").each(function() {
-    const hourBlock = parseInt(this.id);
-    $(this).toggleClass("past", hourBlock < currentHour);
-    $(this).toggleClass("present", hourBlock === currentHour);
-    $(this).toggleClass("future", hourBlock > currentHour);
-  });
-}
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
   // Get the current hour of the day using the dayjs library.
- 
+  const currentHour = dayjs().format("H");
+  // The function below changes the color of each time block based on whether it's in the "past, present, or future" relative to the current hour.
+  function hourlyColor() {
+    $(".time-block").each(function () {
+      const hourBlock = parseInt(this.id);
+      $(this).toggleClass("past", hourBlock < currentHour);
+      $(this).toggleClass("present", hourBlock === currentHour);
+      $(this).toggleClass("future", hourBlock > currentHour);
+    });
+  }
+  // The  function below will save the user's input in a textarea to localStorage - only when the corresponding save button has been clicked.
+  function textEntry() {
+    $('.saveBtn').on('click', function () {
+      const key = $(this).parent().attr('id');
+      const value = $(this).siblings('.description').val();
+      localStorage.setItem(key, value);
+    });
+  }
+  // The function below will refresh the color of each time block based on whether it's in the past(grey), present(red), or future(green) relative to the current time. 
+  function refreshColor() {
+    $('.time-block').each(function () {
+      const hourBlock = parseInt(this.id);
+      if (hourBlock == currentHour) {
+        $(this).removeClass('past future').addClass('present');
+      } else if (hourBlock < currentHour) {
+        $(this).removeClass('future present').addClass('past');
+      } else {
+        $(this).removeClass('past present').addClass('future');
+      }
+    });
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // Get the current hour of the day using the dayjs library.
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
